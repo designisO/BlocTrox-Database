1be8ef4db1c724de52a2d6447da09f136b733248
DesignisO <DesignIsO@protonmail.com>
2022-10-04 12:12:05 -0400

    adding files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reviewing PSQL and Python
+In this repo I wanted to review how to connect to a database using Python using psycopg2. The sql file was created using fictious data on a website called Mockaroo. The goal was to build a simple CRUD application that allowed me to work on the syntax of SQL.
+
---
+++ main.py
@@ -0,0 +1,49 @@
+import psycopg2
+
+# Setting up the bloctrox database.
+conn = psycopg2.connect(database="bloctrox",
+                        host="localhost",
+                        user="postgres",
+                        password="dataisnow21",
+                        port="5432")
+# created a cursor
+cursor = conn.cursor()
+
+# Executing a SQL query to insert data into  table
+insert_query = """ INSERT INTO bloctrox (id, user_name, eth_address) VALUES (101, 'orion3000', '0x455A569b9c7eE32E3EC5dadAe3413f1355972e0D')"""
+
+
+cursor.execute(insert_query)
+conn.commit()
+print("1 Record inserted in BlocTrox Database successfully")
+
+# Fetch result
+cursor.execute("SELECT * FROM bloctrox")
+record = cursor.fetchall()
+print("Result ", record)
+
+# Executing a SQL query to update table
+update_query = """Update bloctrox set user_name = '3000Orion' where id = 90"""
+cursor.execute(update_query)
+conn.commit()
+count = cursor.rowcount
+print(count, "Record updated successfully ")
+
+# Fetch result
+cursor.execute("SELECT * FROM bloctrox")
+print("Result ", cursor.fetchall())
+
+# Deletion of a query
+delete_query = """Delete FROM bloctrox where id = 90"""
+cursor.execute(delete_query)
+conn.commit()
+count = cursor.rowcount
+print(count, "Record deleted successfully ")
+
+# Fetch result
+cursor.execute("SELECT * from bloctrox")
+print("Result ", cursor.fetchall())
+
+
+
+
